cf186c7e7db57c6805911bea6a35c81b34382a30
614788 <rudduck.614788@swl.k12.oh.us>
2024-10-17 08:52:23 -0400

    Initial commit

---
+++ J/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 2; i++){
+            for (int r = 0; r <= 20; r++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i <= 7; i++){
+            System.out.print("       ");
+            for (int r = 0; r <= 5; r++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i <= 2; i++){
+            for (int r = 0; r <= 12; r++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
